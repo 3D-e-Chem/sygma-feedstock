a6230748e59dc420e122890b75fd3a33423b96d9
Lars Ridder <l.ridder@esciencecenter.nl>
2017-02-02 17:41:09 +0100

    update sygma version add rdkit source channel

--- ci_support/run_docker_build.sh
+++ ci_support/run_docker_build.sh
@@ -13,8 +13,6 @@ docker info
 config=$(cat <<CONDARC
 
 channels:
- - conda-forge
- - defaults
  - rdkit
 
 conda-build:
@@ -42,7 +40,16 @@ conda clean --lock
 conda install --yes --quiet conda-forge-build-setup
 source run_conda_forge_build_setup
 
-# Embarking on 1 case(s).
+# Embarking on 2 case(s).
+    set -x
+    export CONDA_PY=27
+    set +x
+    conda build /recipe_root --quiet || exit 1
+    upload_or_check_non_existence /recipe_root 3D-e-Chem --channel=main || exit 1
+
+    set -x
+    export CONDA_PY=35
+    set +x
     conda build /recipe_root --quiet || exit 1
     upload_or_check_non_existence /recipe_root 3D-e-Chem --channel=main || exit 1
 EOF
